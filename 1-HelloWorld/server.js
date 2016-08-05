2ad3a129e3cc95823be405601e44f57c78db0a1b
Burak Donbay <burak.donbay@gmail.com>
2016-08-05 22:24:24 +0300

    Initial commit

--- 1-HelloWorld/server.js
+++ 1-HelloWorld/server.js
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.end('Meteorolojiye Selamlar!\n');
 }).listen(port);
