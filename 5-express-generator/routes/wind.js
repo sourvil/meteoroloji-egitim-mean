8d38a40a2dfe39b45fcdae5ae7d1f53b0d04ad87
Burak Donbay <burak.donbay@gmail.com>
2016-08-07 21:05:04 +0300

    express-generator added

---
+++ 5-express-generator/routes/wind.js
@@ -0,0 +1,52 @@
+﻿var express = require('express');
+var router = express.Router();
+
+var request = require('sync-request');
+const env = require('env2')('./config.env');
+
+var istanbulRuzgar = ruzgarGetir('ISTANBUL');
+var ankaraRuzgar = ruzgarGetir('ANKARA');
+var istanbulIcon = iconGetir('ISTANBUL');
+var ankaraIcon = iconGetir('ANKARA');
+
+function ruzgarGetir(city) {
+
+    var url = "";
+    if (city == 'ISTANBUL')
+        url = process.env.istanbulWurl;
+    else
+        url = process.env.ankaraWurl;
+
+    var res = request('GET', url);
+    var data = JSON.parse(res.getBody('utf-8'));
+    return data.current_observation.wind_string;
+};
+
+function iconGetir(city) {
+
+    var url = "";
+    if (city == 'ISTANBUL')
+        url = process.env.istanbulWurl;
+    else
+        url = process.env.ankaraWurl;
+
+    var res = request('GET', url);
+    var data = JSON.parse(res.getBody('utf-8'));
+    return data.current_observation.icon_url;
+};
+
+
+
+/* GET wind page. */
+router.get('/', function (req, res, next) {
+    res.render('wind', {
+        city: 'ANKARA',
+        cityWind: ankaraRuzgar,
+        cityIcon: ankaraIcon,
+        city2: 'ISTANBUL',
+        cityWind2: istanbulRuzgar,
+        cityIcon2: istanbulIcon
+    });
+});
+
+module.exports = router;
