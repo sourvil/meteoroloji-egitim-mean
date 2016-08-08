2d7e60a3eb43972d6028c9e12825dfa61e24946c
Burak Donbay <burak.donbay@gmail.com>
2016-08-08 22:05:12 +0300

    socket.io project updated

--- 8-socketio/server.js
+++ 8-socketio/server.js
@@ -1,6 +1,17 @@
-﻿var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+﻿var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('message', function (msg) {
+        io.emit('message', msg);
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on port:3000');
+});
