0b916d0d63813bba41e6e8ded5de9004df9b424d
Burak Donbay <burak.donbay@gmail.com>
2016-08-10 22:16:59 +0300

    custom filter added

--- 12-ng-expression-filter/app/index.js
+++ 12-ng-expression-filter/app/index.js
@@ -8,6 +8,15 @@
     console.log("index.run çalıştı");
 })
 
+    .filter('meteorDateFormat', function ($filter) {
+        return function (givenTime) {
+            var _date = $filter('date')(new Date(givenTime),
+                                'MMM dd yyyy - HH:mm:ss');
+
+            return _date.toUpperCase();
+        };
+    })
+
 
 .controller('WeatherCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
     console.log("WeatherCtrlController");
@@ -17,35 +26,35 @@
 
     $scope.cities = [
         {
-            "name":"Ankara",
-            "temperature":6,
-            "description":"Nem yok, sıcaklık fazla"
+            "name": "Ankara",
+            "temperature": 6,
+            "description": "Nem yok, sıcaklık fazla"
         },
         {
-            "name":"Istanbul",
-            "temperature":34,
-            "description":"Nem orta, sıcaklık fazla"
+            "name": "Istanbul",
+            "temperature": 34,
+            "description": "Nem orta, sıcaklık fazla"
         },
         {
-            "name":"Mersin",
-            "temperature":33,
-            "description":"Nem çok, sıcaklık daha da çok"
+            "name": "Mersin",
+            "temperature": 33,
+            "description": "Nem çok, sıcaklık daha da çok"
         },
         {
-            "name":"Adana",
-            "temperature":1,
-            "description":"Burada herkes sıcaktan çıldırmış"
+            "name": "Adana",
+            "temperature": 1,
+            "description": "Burada herkes sıcaktan çıldırmış"
         },
                 {
-            "name":"Trabzon",
-            "temperature":61,
-            "description":"Her yer yemyeşil"
-        },
+                    "name": "Trabzon",
+                    "temperature": 61,
+                    "description": "Her yer yemyeşil"
+                },
                         {
-            "name":"Denizli",
-            "temperature":20,
-            "description":"Sıcak çok, nem orta"
-        },
+                            "name": "Denizli",
+                            "temperature": 20,
+                            "description": "Sıcak çok, nem orta"
+                        },
 
     ];
 
