b14faf7b8bfd1fc04614d5e5e2f1c594d9b7d097
Burak Donbay <burak.donbay@gmail.com>
2016-08-10 19:33:44 +0300

    11-ng-controller-scope added

---
+++ 11-ng-controller-scope/app/content.js
@@ -0,0 +1,8 @@
+﻿angular.module('content', ['index'])
+.config(function () {
+    console.log('content.config çalıştı');
+})
+.run(function () {
+    console.log('content.run çalıştı');
+})
+;
---
+++ 11-ng-controller-scope/app/index.js
@@ -0,0 +1,43 @@
+﻿var index = angular.module('index', ['content'])
+.config(function () {
+    // provider injection
+    console.log("index.config çalıştı");
+})
+.run(function () {
+    // instance injection
+    console.log("index.run çalıştı");
+})
+
+.controller('SeasonCtrl', ['$scope', function ($scope) {
+    console.log('SeasonController');
+
+        $scope.season = 'İlkbahar';
+        this.season = 'Sonbahar';
+
+        $scope.setSeason = function (newSeason) {
+            console.log('Mevsim artık: ' + newSeason);
+            $scope.season = newSeason;
+        };
+}])
+
+.controller('WeatherCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    console.log("WeatherCtrlController");
+    $scope.weather = 'Ilık';
+    $rootScope.currentTime = Date.now();
+
+    $scope.setWeather = function (newWeather) {
+        console.log('Hava artık: ' + newWeather);
+        $scope.weather = newWeather;
+    };
+
+    $scope.resetTime = function(){
+        console.log('resetTime');
+        $rootScope.currentTime = Date.now();
+
+    };
+
+}])
+
+
+
+;
